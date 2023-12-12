aab814b6fc7a0813a2c4b812d78a7535b242acb5
Alejo Pereyra <alejo@alejo.io>
2023-12-11 22:04:28 -0300

    First commit

---
+++ FasterTeleporterPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+
+namespace FasterTeleporter {
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin {
+        internal static Harmony h = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        private void Awake() {
+            Logger.LogInfo($"Loading plugin {PluginInfo.PLUGIN_GUID}");
+            try {
+
+                h.PatchAll(typeof(Patches.TeleporterPatches));
+
+                LC_API.ServerAPI.ModdedServer.SetServerModdedOnly();
+                Logger.LogInfo($"Loaded plugin {PluginInfo.PLUGIN_GUID}");
+            } catch (Exception ex) {
+                Logger.LogInfo($"Error loading {PluginInfo.PLUGIN_GUID} (Exception)");
+                Logger.LogError(ex.Message);
+                Logger.LogError(ex.StackTrace);
+            }
+        }
+    }
+}
---
+++ Patches/TeleporterPatches.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace FasterTeleporter.Patches
+{
+    public static class TeleporterPatches {
+        [HarmonyPatch(typeof(ShipTeleporter), "Awake")]
+        [HarmonyPostfix]
+        static void Awake(bool ___isInverseTeleporter, ref float ___cooldownAmount, ref float ___cooldownTime) {
+            if (___isInverseTeleporter) {
+                ___cooldownAmount = 0;
+                ___cooldownTime = 0;
+            }
+        }
+    }
+}
